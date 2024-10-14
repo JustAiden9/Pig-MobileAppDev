1267d35fa471d868f1620390627cbd127ef8d093
Aiden Baker <aidenbak9@gmail.com>
2024-10-14 09:47:10 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Aiden Baker on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PigTests/PigTests.swift
@@ -0,0 +1,16 @@
+//
+//  PigTests.swift
+//  PigTests
+//
+//  Created by Aiden Baker on 10/3/24.
+//
+
+import Testing
+
+struct PigTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
